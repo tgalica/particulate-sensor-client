dfac8df595dd1bd4d90a8f6adc22559055ad2ed7
mtrusczyk <mtrusczyk@gmail.com>
2019-04-25 17:24:12 -0400

    added LED wrapper

---
+++ RgbLed.py
@@ -0,0 +1,30 @@
+import board
+from digitalio import DigitalInOut, Direction
+
+class RgbLed:
+    def __init__(self, redPin, greenPin, bluePin):
+        self.redPin = DigitalInOut(redPin)
+        self.redPin.direction = Direction.OUTPUT
+        self.bluePin = DigitalInOut(bluePin)
+        self.bluePin.direction = Direction.OUTPUT
+        self.greenPin = DigitalInOut(greenPin)
+        self.greenPin.direction = Direction.OUTPUT
+
+
+    def turnOn(self, red, green, blue):
+        if(green):
+            self.greenPin.value(1)
+        else:
+            self.greenPin.value(0)
+        if(blue):
+            self.bluePin.value(1)
+        else:
+            self.bluePin.value(0)
+        if(red):
+            self.redPin.value(1)
+        else:
+            self.bluePin.value(0)
+    def turnOff(self):
+        self.bluePin.value(0)
+        self.greenPin.value(0)
+        self.redPin.value(0)
