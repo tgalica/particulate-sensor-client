91ea649838770e776f3f642722a0fb694006ce9a
Thomas Galica <tgalica@pop-os.localdomain>
2019-04-26 09:59:58 -0400

    cleanup commented code

--- transmitter.py
+++ transmitter.py
@@ -3,7 +3,6 @@ from config import config
 import pyrebase
 
 import json
-# import jsonpickle
 
 class Transmitter:
     def __init__(self):
@@ -15,11 +14,7 @@ class Transmitter:
 
         db = firebase.database()
 
-        # airborn_json = jsonpickle.encode(airborne_data)
         payload = json.dumps([x.getObjectAsDictionary() for x in airborne_data])
-        # print(json.dumps(payload))
-        # print(airborne_json)
-        # print(data)
         db.child("airborne-data").push(payload)
         
         test = db.child("airborne-data").get()
