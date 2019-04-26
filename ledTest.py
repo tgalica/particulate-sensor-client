19df79f237f051126e372dbcb8cd6aa4639f6177
Matt Rusczyk <mtrusczyk@gmail.com>
2019-04-26 11:11:00 -0400

    comments

--- ledTest.py
+++ ledTest.py
@@ -4,12 +4,16 @@ import board
 
 class ledTest:
     led = RgbLed(board.D17,board.D27,board.D22)
+    print ("white")
     led.turnOn(red=True, green=True, blue=True)
     sleep(1)
+    print ("red")
     led.turnOn(red=True, green=False, blue=False)
     sleep(1)
+    print ("green")
     led.turnOn(red=False, green=True, blue=False)
     sleep(1)
+    print ("blue")
     led.turnOn(red=False, green=False, blue=True)
     sleep(1)
     led.turnOff
